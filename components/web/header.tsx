24a06747a22cb7a78c6bb231bd866e50dbd2f454
rajsam <glitternail20@gmail.com>
2026-06-03 08:19:23 +0530

    final

--- components/web/header.tsx
+++ components/web/header.tsx
@@ -28,19 +28,19 @@ const Header = () => {
 
 
 
-    <div className = "relative w-[1200px] h-48 md:h-100 lg:h-120 rounded-lg p-4 bg-gray-100 shadow-md mx-auto overflow-hidden">
+    <div className = "relative w-full h-48 relative w-full h-48 mt-8 max-w-3xl w-[300px] sm:w-[300px] sm:h-64 md:w-[600px] md:h-[400px] lg:w-[1200px] lg:h-[500px  rounded-lg p-4 bg-gray-100 shadow-md mx-auto overflow-hidden">
 
-    <Image src="/mainimage.avif" alt="Main Image" className=" objectfit-cover" fill/>
+    <Image src="/mainimage.avif" alt="Main Image" className=" ]"  fill/>
         
         
         
     </div >
 
-    <div className="mt-8 max-w-3xl rounded-lg py-6 bg-[#f0f0f0] mx-auto flex justify-center items-center">
+    <div className=" rounded-lg py-6 bg-[#f0f0f0] mx-auto flex justify-center items-center">
       
       
       
-      <h1 className="text-5xl text-black tracking-tight mb-6">Start Planning Your Journey !</h1>      
+      <h1 className="text-3xl  text-black tracking-tight mb-6 h-auto ] text-center">Start Planning Your Journey !</h1>      
 
 
 
